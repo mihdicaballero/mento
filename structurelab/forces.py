c49f87b0646da8433acf6bf4fe8ed9095763aa5a
JuanPabloRomaris <114318627+JuanPabloRomaris@users.noreply.github.com>
2024-07-09 22:29:15 -0300

    Update forces.py
    Agrego unidades de forallpeople a Forces

--- structurelab/forces.py
+++ structurelab/forces.py
@@ -1,9 +1,22 @@
+from dataclasses import dataclass
+import forallpeople
+
+forallpeople.environment('structural', top_level=True)
+
+@dataclass
 class Forces:
-    def __init__(self, My=0.0):
-        self.My = My
+    My: float = 0 * kN * m  # type: ignore
 
     def get_moment(self):
         return self.My
 
     def set_moment(self, My):
         self.My = My
+
+
+# Ejemplo de uso de la clase Forces
+f1 = Forces() #Creando con valor por defecto
+print(f1.get_moment())  # Imprimirá 0 kN·m
+
+f2 = Forces(10 * kN * m) # Creando con argumento
+print(f2.get_moment())  # Imprimirá 10 kN·m
