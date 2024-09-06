19b9cbfe94978b42d2182c9fa733c65fc03cceb4
JuanPabloRomaris <114318627+JuanPabloRomaris@users.noreply.github.com>
2024-09-05 21:56:30 -0300

    New flexural design

--- structurelab/section.py
+++ structurelab/section.py
@@ -83,7 +83,8 @@ class Beam(RectangularConcreteSection):
 
 
 
-    def design_flexure_ACI_318_19(self, M_u:float):
+    def design_flexure_ACI_318_19_deprecated(self, M_u:float):
+        # This method was based on iterative approach and is deprecated, now we solve the quadratic equation
         a_assumed=0.08*self._depth # Adopted value to be confirmed
 
         concrete_properties=self.concrete.get_properties()
@@ -129,6 +130,44 @@ class Beam(RectangularConcreteSection):
             print(f"PARA EL MOMENTO {M_u}, EL ARMADO DE CALCULO ES {self.__A_s_calculated}")
 
 
+    def design_flexure_ACI_318_19(self, M_u:float):
+        phi=0.9 # ACI 318 2019 Indicates that beams must work dominated by tension
+        concrete_properties=self.concrete.get_properties()
+        f_c=concrete_properties["f_c"]
+        rebar_properties=self.steelBar.get_properties()
+        f_y=rebar_properties["f_y"]
+        d=0.9*self._depth # Asumption
+        A=-phi*f_y**2/(1.7*f_c*self._width)
+        B=phi*f_y*d
+        C=-M_u
+        A_s_calc=(-B+np.sqrt(B**2 - 4*A*C))/(2*A)
+
+        A_s_min=max((0.25*np.sqrt(f_c / MPa)*MPa/f_y*self._depth*self._width/cm**2) , (1.4*MPa/f_y*self._depth*self._width/cm**2))*cm**2# type: ignore
+
+        settingMISTERIOSOS= False # ACA VER ESTO DESPUES, ES PARA LO DE LA REDUCCIOn
+
+        if A_s_calc>A_s_min:
+            self.__A_s_calculated=A_s_calc
+        elif  4*A_s_calc/3 > A_s_min:
+            self.__A_s_calculated=A_s_min
+        else: 
+            if settingMISTERIOSOS:
+                self.__A_s_calculated=4*A_s_calc/3
+            else:
+                self.__A_s_calculated=A_s_min
+            
+        rho_max=self.__determine_maximum_flexural_reinforcement_ratio_ACI_318_19()
+        A_s_max=rho_max*self._depth*self._width
+        if self.__A_s_calculated > A_s_max:
+            print(f"El armado requerido es mayor al maximo")
+        else:
+            print(f"PARA EL MOMENTO {M_u}, EL ARMADO DE CALCULO ES {self.__A_s_calculated}")
+
+
+
+
+
+
     '''
     def desgin_flexure():
         if isinstance(self.concrete, concreteACI):
@@ -140,6 +179,7 @@ class Beam(RectangularConcreteSection):
     def check_flexure_ACI_318_19(self):
         max_rho=self.__determine_maximum_flexural_reinforcement_ratio_ACI_318_19()
         print(max_rho)
+        
         pass        
 
 
@@ -158,6 +198,7 @@ def main():
 
     print(f"Nombre de la sección: {section.get_name()}")
     section.design_flexure_ACI_318_19(500*kN*m)  # type: ignore
+    section.design_flexure_ACI_318_19_deprecated(500*kN*m)  # type: ignore
 
 if __name__ == "__main__":
     main()
