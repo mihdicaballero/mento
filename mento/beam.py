f1d67678b367306c734eccef47722c9f624dffd2
JuanPabloRomaris <114318627+JuanPabloRomaris@users.noreply.github.com>
2025-03-03 13:36:52 -0300

    Update beam.py

--- mento/beam.py
+++ mento/beam.py
@@ -2416,44 +2416,10 @@ def shear_CIRSOC() -> None:
 if __name__ == "__main__":
     # flexure_check_test()
     # flexure_design_test()
-    #flexure_design_test_calcpad_example() 
+    flexure_design_test_calcpad_example() 
     # flexure_Mn()
     # shear_ACI_imperial()
     # shear_EN_1992()
     # rebar()
     # shear_ACI_metric()
     # shear_CIRSOC()
-    from mento import Concrete_ACI_318_19, SteelBar, RectangularConcreteBeam
-    from mento import psi, inch, ksi
-    from mento.section import Section
-    from mento.forces import Forces
-    from mento.node import Node
-
-    # Define concrete and steel materials
-    concrete = Concrete_ACI_318_19(name="C4", f_c=4000 * psi)
-    steel = SteelBar(name="fy=6000", f_y=60 * ksi)
-
-    # Initialize section using default settings
-    section = RectangularConcreteBeam(
-        label="B-12x24",
-        concrete=concrete,
-        steel_bar=steel,
-        width=12 * inch,
-        height=24 * inch
-    )
-
-    # Initialize the flexural solicitations of the section
-    f1 = Forces(label='Combo_01', M_y=400*kip*ft)
-    f2 = Forces(label='Combo_02', M_y=-400*kip*ft)
-    f3 = Forces(label='Combo_03', M_y=37*kip*ft)
-    f4 = Forces(label='Combo_04', M_y=150*kip*ft)
-    f5 = Forces(label='Combo_05', M_y=-1*kip*ft)
-    
-    # Create the node that associates the section with the forces
-    Node(section=beam, forces=[f1,f2,f2,f4,f5])
-
-    # Dimensionate the steel rebar required for the beam:
-    # As the concrete was defined using Concrete_ACI_318_19, the code for design will be ACI 318 19
-    flexure_results = beam.design_flexure()
-    print(flexure_results)
-
